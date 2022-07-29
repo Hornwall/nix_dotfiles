3d56eeb29d040d3e0f96850177ffbe4310e587af
Hannes Hornwall <hannes@hornwall.me>
2022-07-29 09:17:18 +0200

    Fix nix config

--- configuration.nix
+++ configuration.nix
@@ -58,7 +58,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.hannes = {
     isNormalUser = true;
-    extraGroups = [ "wheel", "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
   };
 
   # List packages installed in system profile. To search, run:
